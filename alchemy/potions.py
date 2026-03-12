73223e60c4673d9f4f3edb411ffeb95e0fbf1408
Abdelmoghit Redouane <aredouan@e1r6p15.1337.ma>
2026-03-12 02:23:54 +0000

    done

---
+++ alchemy/potions.py
@@ -0,0 +1,21 @@
+from .elements import (create_fire, create_water,
+                       create_earth, create_air)
+
+
+def healing_potion() -> str:
+    return f"Healing potion brewed with {create_fire()} and {create_water()}"
+
+
+def strength_potion() -> str:
+    return f"Strength potion brewed with {create_earth()} and {create_fire()}"
+
+
+def invisibility_potion() -> str:
+    return (f"Invisibility potion brewed with {create_air()} "
+            f"and {create_water()}")
+
+
+def wisdom_potion() -> str:
+    elements_combined = (f"{create_fire()}, {create_water()}, "
+                         f"{create_earth()}, {create_air()}")
+    return f"Wisdom potion brewed with all elements: {elements_combined}"
